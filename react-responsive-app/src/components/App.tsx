da6b559b6b681861143ba7bf3cd76900d99b4319
Romain <romain.pietri@student.junia.com>
2025-04-24 11:28:50 +0200

    Comit React

---
+++ react-responsive-app/src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <h1>Welcome to the Responsive React App</h1>
+            <p>This is a basic responsive front-end application built with React.</p>
+        </div>
+    );
+};
+
+export default App;
